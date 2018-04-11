a0572e9d1976b75aec9521fcf580055377180b00
Andrew Laramee <zephix109@gmail.com>
2018-04-11 11:22:46 -0400

    Local host chatbox functioning

--- 445_A3/src/Chat_Client.java
+++ 445_A3/src/Chat_Client.java
@@ -1,21 +1,29 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.net.SocketException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Chat_Client {
 
 	public static void main(String[] args) {
 		
-		DatagramSocket socket = new DatagramSocket();
-        ChatReceiver chatR = new ChatReceiver(socket);
-        ChatSender chatS = new ChatSender(socket);
-        Thread receiverThread = new Thread(chatR);
-        Thread senderThread = new Thread(chatS);
-        receiverThread.start(); 
-        senderThread.start();
+		try {
+			DatagramSocket socket = new DatagramSocket(5555);
+			ChatReceiver chatR = new ChatReceiver(socket);
+	        ChatSender chatS = new ChatSender(socket);
+	        Thread receiverThread = new Thread(chatR);
+	        Thread senderThread = new Thread(chatS);
+	        receiverThread.start(); 
+	        senderThread.start();
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+        
 	}
 
 }
@@ -25,6 +33,7 @@ class ChatSender implements Runnable {
 	final static String ip_address = "127.0.0.1";
 	final static int port = 5555;
 	private DatagramSocket senderSocket;
+	String username;
 
 	ChatSender(DatagramSocket s) {
         senderSocket = s;
@@ -37,16 +46,58 @@ class ChatSender implements Runnable {
 		senderSocket.send(packet);
 	}
 
+	@Override
 	public void run() {
 		
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("Welcome, please enter your name: ");
+		try {
+			username = in.readLine();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
+        while (true) {
+            try {
+            	String line = in.readLine();
+            	if(line.equals("quit")) {
+            		System.out.println("Closing chatbox");
+            		System.exit(0);
+            	}
+                Date date = new Date();
+                SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy h:mm:ss a");
+                String formattedDate = sdf.format(date);                
+                sendMessage(formattedDate + " [" + username + "]: " + line);
+            } catch(Exception e) {
+                System.err.println(e);
+            }
+        }
 	}
 }
 
 class ChatReceiver implements Runnable {
 
-	@Override
-	public void run() {
-
-	}
+	DatagramSocket receiverSocket;
+    byte buffer[];
+    
+    ChatReceiver(DatagramSocket s) {
+        receiverSocket = s;
+        buffer = new byte[1024];
+    }
+    
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                receiverSocket.receive(packet);
+                String received = new String(packet.getData(), 0, packet.getLength());
+                System.out.println(received);
+            } catch(Exception e) {
+                System.err.println(e);
+            }
+        }
+    }
 
 }
